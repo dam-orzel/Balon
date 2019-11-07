58d6f20c3c69bcfb809c799d613d2bab0ed6ab0f
Damian Orzeł <dam.orzel@gmail.com>
2019-11-07 14:22:50 +0100

    bug fix and new features added

--- main.js
+++ main.js
@@ -1,33 +1,29 @@
-$(document).ready(function() {
-  $(".carousel").slick({
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    arrows: true,
-    dots: false,
-    prevArrow: $(".prev"),
-    nextArrow: $(".next"),
-    responsive: [
-      {
-        breakpoint: 1424,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 1124,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
+function equalHeight(group) {
+  tallest = 0;
+  group.each(function() {
+    thisHeight = $(this).height();
+    if (thisHeight > tallest) {
+      tallest = thisHeight;
+    }
   });
+  group.height(tallest);
+}
+
+$(document).ready(function() {
+  equalHeight($(".item-content"));
+});
+
+function equalHeight(group) {
+  tallest = 0;
+  group.each(function() {
+    thisHeight = $(this).height();
+    if (thisHeight > tallest) {
+      tallest = thisHeight;
+    }
+  });
+  group.height(tallest);
+}
+
+$(document).ready(function() {
+  equalHeight($(".item-news-content"));
 });
